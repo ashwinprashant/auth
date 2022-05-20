670d62b1002ee076a3f033ca9e32fa1fd24860fc
ashwinprashant <ashwin.prashant@kellogg.northwestern.edu>
2022-05-20 19:27:11 +0000

    auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+    def new 
+    end
+
+
+    def create
+    #Step 1: check to see if there's a user with email that was provided 
+    @user = User.find_by({ "email" => params["email"] })
+    if @user 
+        #Step 1a: If yes, check the password (go to step 2). 
+    #Step 2: Check the password to see if it matches the user's password
+        if params["password"] == @user["password"]
+        flash["notice"] = "You're in!"
+        #Step 2a: If yes, go to companies page.
+            redirect_to "/companies"
+        else
+    #Step 2b: If no, go back to the loging page. 
+            flash["notice"] = "Incorrect credentials"
+            redirect_to "/sessions/new"
+        end
+    else
+    #Step 1b: If no, go back to the login page
+        flash["notice"] = "Incorrect credentials"
+        redirect_to "/sessions/new"
+    end
+end
+end
